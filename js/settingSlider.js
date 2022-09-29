146eea5b8952c3d4adf8a44abb28aef1b41120e1
AzAlex <99219332+azalex020204@users.noreply.github.com>
2022-09-29 03:23:07 -0700

    Add files via upload

---
+++ js/settingSlider.js
@@ -0,0 +1,71 @@
+$(function () {
+  $('.slider1').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: true,
+    fade: true,
+    asNavFor: '.slider2'
+  });
+  $('.slider2').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider1',
+    infinite: true,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: true,
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1131,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          vertical: false,
+          verticalSwiping: false,
+          centerMode: false,
+
+        }
+      },
+    ]
+  });
+
+   $('.sliderUseges').slick({
+    slidesToShow: 3,
+    infinite: true,
+    slidesToScroll: 1,
+    dots: false,
+    focusOnSelect: false,
+    rows: 2,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 1131,
+        settings: {
+          rows: 1,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          rows: 1,
+        }
+      },
+    ]
+  });
+})
